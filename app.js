195b1b8f5a65f4efbd6f885481fd7a600ae65458
harry <denton.harry@gmail.com>
2020-03-14 10:02:23 +0000

    deleted second function which wouldn't work after 2 hours.

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
+const 
+
+
+
+
+
+
 function computerPlay() {
 
-    let choices = ["r", "p", "s"];
+    let computerChoice = ["r", "p", "s"];
   
     let randomNumber = Math.floor(Math.random() * 3);
   
-    return choices [randomNumber];
+    return computerChoice [randomNumber];
   }
-  
- console.log(computerPlay());
-
- function displayUserChoice() { 
-  var ele = document.getElementsByName("player-selection"); 
- }
-
- alert(displayUserChoice());
