41d2748605e99da46dca8f17464b94a4dceec51f
harry <denton.harry@gmail.com>
2020-04-10 11:22:32 +0100

    sorted html and css

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let rockDiv = document.getElementById("button-rock");
 let paperDiv = document.getElementById("button-paper");
 let scissorsDiv = document.getElementById("button-scissors")
 
+let userScore = 0;
+let computerScore = 0;
+
 function computerPlay() {
 
     let computerChoice = ["rock", "paper", "scissors"];
