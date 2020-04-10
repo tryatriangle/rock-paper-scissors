c3aa90d7aea335d91961868b74c4a3227572b74f
harry <denton.harry@gmail.com>
2020-04-10 12:27:01 +0100

    started on javascript again

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-let rockDiv = document.getElementById("button-rock");
-let paperDiv = document.getElementById("button-paper");
-let scissorsDiv = document.getElementById("button-scissors");
+
+//buttons
+let rock_div = document.getElementById("button-rock");
+let paper_div = document.getElementById("button-paper");
+let scissors_div = document.getElementById("button-scissors");
+
+//score and result
 let userScore_span = document.getElementById("user-score");
 let computerScore_span = document.getElementById("computer-score");
-
-
+let scoreBoard_div = document.querySelector(".score-board");
+let result_div = document.querySelector(".result")
 let userScore = 0;
 let computerScore = 0;
+
+//event listeners
+
+//functions
 
 function computerPlay() {
 
